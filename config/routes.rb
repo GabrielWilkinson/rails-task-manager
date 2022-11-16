c7516508e05cf99e1197869ea90b2edc240135a3
Gabriel Wilkinson <gabriel.wilkinson@hotmail.com>
2022-11-16 12:09:39 +0000

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "tasks", to: "tasks#index"
-
-  get "tasks/new", to: "tasks#new"
-  post "tasks", to: "tasks#create"
-
-  delete "tasks/:id", to: "tasks#destroy"
-
-  patch "tasks/:id", to: "tasks#update"
-
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit
-  get "tasks/:id", to: "tasks#show", as: :task
-
-
-
-
 end
